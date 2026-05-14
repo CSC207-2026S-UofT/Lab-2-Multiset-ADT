e732e98e31f4e1dfd9ada26b81ec84803bb3e1f3
Edwz208 <edwinzeng99@gmail.com>
2026-05-14 19:37:50 -0400

    Initial tree methods

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,50 @@
+import java.util.ArrayList;
+
 public class Tree {
-    // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+    private Integer root;
+    private ArrayList<Tree> subtrees;
+
+    public Tree(Integer root, ArrayList<Tree> subtrees) {
+        this.root = root;
+        if (subtrees.isEmpty()){
+            subtrees = null;
+        }
+        else {
+            this.subtrees = subtrees;
+        }
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    private int len() {
+        if (this.root == null) {
+            return 0;
+        }
+        else {
+            int size = 1;
+            for (Tree subtree : subtrees) {
+                size += subtree.len();
+            }
+            return size;
+        }
+    }
+
+    public int count(Integer item) {
+        if (this.root == null) {
+            return 0;
+        }
+        else {
+            int num = 0;
+            if (this.root.equals(item)) {
+                num += 1;
+            }
+            for (Tree subtree : subtrees) {
+                num += subtree.count(item);
+            }
+            return num;
+        }
+    }
+
 }
